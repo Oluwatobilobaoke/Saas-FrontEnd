cc4794e82304c28b9e741b9b09b26733e5a54d1c
Bello <gbahdeybohbello@gmail.com>
2020-03-13 02:32:12 +0100

    Modified category selection on account sign up to allow users only select a category once data is loaded from the database and added a few comments to the codebase to made it readable

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,43 +1,53 @@
-let url = "https://stuaas.herokuapp.com/api/v1/";
+// ============================================================================================
+// | All the code here has been moved to signup-validation.js with some few modifications made|
+//  ===========================================================================================
 
-document.addEventListener("DOMContentLoaded", () => {
-	let selectUserCategory = document.querySelector('#selectUserCategory');
-	selectUserCategory.addEventListener("change", () => {
-		let studentAccountCreationSection = document.querySelector("#studentAccountCreationSection");
-		let organizationAccountCreationSection = document.querySelector("#organizationAccountCreationSection");
-		let userCategorySelected = selectUserCategory.value;
-		console.log("The selected category is ====>> ", userCategorySelected)
-		let createAccountBtn = document.querySelector('#createAccountBtn');
-		if(userCategorySelected === 'organization'){
-			createAccountBtn.removeAttribute("disabled");
-			// Fetch the category for organizations 
-			fetch(`${url}organizations/categories`)
-			.then(res => res.json())
-			.then(res => {
-				studentAccountCreationSection.style.display = "none";
-				organizationAccountCreationSection.style.display = "block";
-				if(res.success){
-					let datas = res.payload.data;
-					datas.forEach(data => {
-						let categories = document.querySelector('#companyCategories');
-						categories.innerHTML += `<option value="${data.name}" data-id="${data.id}">${data.name}</option>`
-					})
-				}
-			})
-			.catch(err => {
-				console.log("The error is => ", err);
-			})
-		}
-		else if(userCategorySelected === "0"){
-			createAccountBtn.setAttribute("disabled", '');
-			organizationAccountCreationSection.style.display = "none";
-			studentAccountCreationSection.style.display = "none";
-		}
-		else{
-			createAccountBtn.removeAttribute("disabled");
-			organizationAccountCreationSection.style.display = "none";
-			studentAccountCreationSection.style.display = "block";
-		}
-		console.log(userCategorySelected);
-	})
-})
+
+
+
+
+// let url = "https://stuaas.herokuapp.com/api/v1/";
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 	let selectUserCategory = document.querySelector('#selectUserCategory');
+// 	selectUserCategory.addEventListener("change", () => {
+// 		let studentAccountCreationSection = document.querySelector("#studentAccountCreationSection");
+// 		let organizationAccountCreationSection = document.querySelector("#organizationAccountCreationSection");
+// 		let userCategorySelected = selectUserCategory.value;
+// 		console.log("The selected category is ====>> ", userCategorySelected)
+// 		let createAccountBtn = document.querySelector('#createAccountBtn');
+// 		if(userCategorySelected === 'organization'){
+// 			createAccountBtn.removeAttribute("disabled");
+// 			// Fetch the category for organizations 
+// 			fetch(`${url}organizations/categories`)
+// 			.then(res => res.json())
+// 			.then(res => {
+// 				studentAccountCreationSection.style.display = "none";
+// 				organizationAccountCreationSection.style.display = "block";
+// 				if(res.success){
+// 					let datas = res.payload.data;
+// 					datas.forEach(data => {
+// 						let categories = document.querySelector('#companyCategories');
+// 						categories.innerHTML += `<option value="${data.name}" data-id="${data.id}">${data.name}</option>`
+// 					})
+// 				}
+// 			})
+// 			.catch(err => {
+// 				console.log("The error is => ", err);
+// 			})
+// 		}
+// 		else if(userCategorySelected === "0"){
+// 			createAccountBtn.setAttribute("disabled", '');
+// 			organizationAccountCreationSection.style.display = "none";
+// 			studentAccountCreationSection.style.display = "none";
+// 		}
+// 		else{
+// 			createAccountBtn.removeAttribute("disabled");
+// 			organizationAccountCreationSection.style.display = "none";
+// 			studentAccountCreationSection.style.display = "block";
+// 		}
+// 		console.log(userCategorySelected);
+// 	})
+// })
+
+
